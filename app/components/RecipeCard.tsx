093a8a8eab6a9beb9b52270f535e33f28d1060f4
AnwarAsry <anwareducation03@gmail.com>
2025-08-26 19:22:13 +0200

    done

--- app/components/RecipeCard.tsx
+++ app/components/RecipeCard.tsx
@@ -3,18 +3,19 @@ import { HiOutlineClock } from "react-icons/hi"
 
 export const RecipeCard = () => {
     return (
-        // <a href="#" className="w-fit h-fit">
-        <div className="relative max-w-3xs rounded-xl shadow-md overflow-visible bg-white">
+        <div className="relative max-w-3xs p-4 rounded-xl shadow-md overflow-visible bg-white">
             {/* Image */}
             <img
-                className="w-50 h-40 mx-auto object-cover rounded-xl "
+                className="w-full h-40 mb-4 object-cover rounded-md"
                 src="../../public/stock.png"
                 alt="Recipe Image" />
 
             {/* Content */}
-            <div className="p-4">
+            <div className="">
                 {/* Title */}
-                <h3 className="text-md font-semibold text-gray-800">Spaghetti Carbonara</h3>
+                <a href="#" className="hover:underline">
+                    <h3 className="text-md font-semibold text-gray-800">Spaghetti Carbonara</h3>
+                </a>
 
                 {/* Prep Time */}
                 <div className="w-full mt-2 flex items-center gap-1">
@@ -28,24 +29,5 @@ export const RecipeCard = () => {
                 </div>
             </div>
         </div>
-        // </a>
-
-
-        // <div className="relative bg-black text-white max-w-md w-full rounded-lg px-6 pb-8 pt-16">
-
-
-
-        //     <img
-        //         src="../../public/stock.png"
-        //         alt="Example"
-        //         className="absolute -top-22 left-1/2 -translate-x-1/2 w-3/4 rounded-md shadow-lg"
-        //     />
-
-        //     <p className="text-center text-lg leading-relaxed mt-26">
-        //         Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-        //         sed do eiusmod tempor incididunt ut labore et dolor
-        //     </p>
-        // </div>
-
     )
 }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -12,6 +12,5 @@ export function meta({ }: Route.MetaArgs) {
 export default function Home() {
   return <>
     <Header />
-    <RecipeCard />
   </>;
 }
